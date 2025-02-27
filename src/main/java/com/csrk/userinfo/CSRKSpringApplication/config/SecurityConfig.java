8a4120b90387304485799e758d5204fcc5a474b3
SivaMaddireddy <maddireddynu@gmail.com>
2025-02-27 09:05:21 -0500

    Initial Commit Removed Login To Permit

--- src/main/java/com/csrk/userinfo/CSRKSpringApplication/config/SecurityConfig.java
+++ src/main/java/com/csrk/userinfo/CSRKSpringApplication/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,7 +16,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
@@ -24,6 +25,7 @@ public class SecurityConfig {
                         .loginProcessingUrl("/api/auth/login")
                         .defaultSuccessUrl("/api/auth/success", true)
                         .failureUrl("/api/auth/login?error=true")
+                        .permitAll()
                 )
                 .logout(logout -> logout
                         .logoutUrl("/api/auth/logout")
